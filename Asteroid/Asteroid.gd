878f4608780a9e93a17afa07446fd8c2f763c723
noagird <112106355+noagird@users.noreply.github.com>
2022-09-13 14:18:41 -0400

    Completes exercise

---
+++ Asteroid/Asteroid.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+var velocity = 0
+var initial_speed = 3.0
+
+
+func _ready():
+	velocity = Vector2(0, initial_speed*randf()).rotated(PI*2*randf())
+	
+func _physics_process(delta):
+	position = position + velocity
+	position.x = wrapf(position.x, 0, 1024)
+	position.y = wrapf(position.y, 0, 600)
